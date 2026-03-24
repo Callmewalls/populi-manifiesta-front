1d4756823a055b2d7a6e7af7b206070a6f89dcfb
Fernando <paredesmorafernando@gmail.com>
2026-03-24 10:52:16 +0100

    lost track of generateddd connectors

--- src/app/connectors/document/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './default.service';
-import { DefaultService } from './default.service';
-export * from './documents.service';
-import { DocumentsService } from './documents.service';
-export const APIS = [DefaultService, DocumentsService];
